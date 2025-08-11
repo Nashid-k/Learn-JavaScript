8548df0f18dc5027cb0ff1b477911be44393940c
Nashid-k <nashidk1999@gmail.com>
2025-08-11 19:51:44 +0530

    js_rewind aug 11 2025

--- index.js
+++ index.js
@@ -12,3 +12,108 @@
 // }
 
 // console.log(`My grade is ${myCurrentGrade}, the minimum to qualify is ${passingGrade}. Result: ${result}`);
+
+
+
+// const temperature = 8;
+// const weather = 'summer';
+
+// if(temperature > 90) {
+//     console.log('its hot wear shorts and a t-shirt');
+    
+// }else if(temperature < 30){
+//     console.log('its cold wear jacket');
+    
+// }else if(weather ==='rainy'){
+//     console.log('its rainy wear rain coats');
+    
+// }else{
+// console.log('normal weather, nomral cloths is prefered');
+
+// }
+
+
+// console.log(`temperature :${temperature}°F , weather :${weather}`);
+
+
+
+/*
+Create a program that determines if someone can rent a car. 
+They need to be at least 21 years old, have a valid license, 
+and either have insurance OR be willing to buy the rental company's insurance.
+*/ 
+
+// const customerAge = 21;
+// const hasValiedLicense = true;
+// const hasInsurance = false;
+// const willingToBuyCompanyInsurance = true;
+
+
+// const canRentCar = customerAge >= 21  && hasValiedLicense && ( hasInsurance || willingToBuyCompanyInsurance)
+// // console.log(canRentCar);
+// if(canRentCar){
+//     console.log('Enjoy your ride with us');
+    
+// }else{
+//     console.log('sorry you cant rent the car');
+    
+// }
+
+
+// fruits = ["banana", "orange"]
+// fruits.push('apple');
+// console.log(fruits);
+
+// let task = ["task1","task2","task3"];
+// let lastItem = task.pop()
+// console.log(lastItem);
+
+
+// let queue = [1,2,3,4];
+// let firstElement = queue.shift();
+// console.log(queue);
+
+
+// let message = ["msg1","msg2","msg3"]
+// let firstMessage = message.shift();
+// console.log(message);
+
+// let todos = ["normal","low"]
+// todos.unshift("priority")
+// console.log(todos);
+
+
+// let nums = [1,2,3,4,5]
+// nums.splice(1,2)
+// console.log(nums);
+
+// let songs = ["song1","song2","song3","song4"];
+// songs.splice(2,2)
+// console.log(songs);
+
+
+// let arr = ['a','b','c']
+// arr.splice(1,1,"replaced")
+// console.log(arr);
+
+
+
+// let user = [
+//     {name:'user1',age:20},
+//     {name:'user2',age:33}
+// ]
+
+// let newUser = {name:'newUser',age:18}
+// user.splice(0,1,newUser)
+// console.log(user);
+
+
+
+// let arr = [0,1,2,3,4,5,6]
+// let extElem = arr.slice(1,3)
+// console.log(extElem);
+
+
+
+
+
