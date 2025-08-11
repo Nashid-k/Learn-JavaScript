cb16dc89b8e19e7b9185ff55580302d2e26c7566
Nashid-k <nashidk1999@gmail.com>
2025-08-11 14:54:29 +0530

    js_booleans && comparisons

---
+++ index.js
@@ -0,0 +1,14 @@
+// const myCurrentGrade = 17;
+// const passingGrade   = 25;
+
+// let result;
+
+// if (myCurrentGrade === 100) {
+//   result = 'Perfect score! 🎯';
+// } else if (myCurrentGrade >= passingGrade) {
+//   result = 'Passed ✅';
+// } else {
+//   result = 'Failed ❌';
+// }
+
+// console.log(`My grade is ${myCurrentGrade}, the minimum to qualify is ${passingGrade}. Result: ${result}`);
